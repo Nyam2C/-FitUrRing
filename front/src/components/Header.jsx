3bebe5f488b46bece3be63def8d11ca9d64eb31f
asdfasdf001234 <asdfasdf001234@ajou.ac.kr>
2024-11-17 23:46:13 +0900

    feat: Footer 구현

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,21 +1,21 @@
 import react from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import './Header.css';
+import './index.css';
 import {ReactComponent as Logo} from '../assets/logo.svg';
 
 function MyPage(props){
     if (props.userId){
         return (
             <span>
-                <Link class="link" to='/mypage'>My Page</Link>
-                <Link class="link" to='/signout'>Sign Out</Link>
+                <Link class="link large" to='/mypage'>My Page</Link>
+                <Link class="link large" to='/signout'>Sign Out</Link>
             </span>
         );
     }
     else{
         return (
-            <Link class="link" to='/signin'>Sign in</Link>
+            <Link class="link large" to='/signin'>Sign in</Link>
         );
     }
 }
@@ -24,13 +24,13 @@ function Header(){
 
     return (
             <div id="header">
-                <Link class="logo link" to='/'> 
+                <Link class="logo link large" to='/'> 
                     <Logo width='25pt' height='30pt' fill='#0072CE'/>
                     <span>Fiturring</span> 
                 </Link>
-                <Link class="link" to='/workout'>Workout</Link>
-                <Link class="link" to='/habitTracker'>Habit Tracker</Link>
-                <Link class="link" to='/routine'>Routine</Link>
+                <Link class="link large" to='/workout'>Workout</Link>
+                <Link class="link large" to='/habitTracker'>Habit Tracker</Link>
+                <Link class="link large" to='/routine'>Routine</Link>
                 <MyPage />
             </div>
     );
