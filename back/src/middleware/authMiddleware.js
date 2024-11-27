9a718872360a566a5ea8ab0e53b35757558cbe1f
BakoD <rudghrnt@ajou.ac.kr>
2024-11-27 17:49:23 +0900

    Init: Initialize backend format

---
+++ back/src/middleware/authMiddleware.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = {
+    authenticate: (req, res, next) => {
+    },
+
+    authorize: (req, res, next) => {
+    }
+};
+
+module.exports = authMiddleware;
