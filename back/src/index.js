df31132b2130813044ed1a10305d9621213e8754
BakoD <rudghrnt@ajou.ac.kr>
2024-12-03 15:45:13 +0900

    Resolve: Fix merge conflict

--- back/src/index.js
+++ back/src/index.js
@@ -47,13 +47,9 @@ const habitRouter = require('./routers/habittrackerRouter');
 const routineRouter = require('./routers/routineRouter');
 
 app.use('/api/user', userRouter);
-<<<<<<< HEAD
 app.use('/api/video', videoRouter);
 app.use('/api/habitTracker', habitRouter);
 app.use('/api/routine', routineRouter);
-=======
-app.use('/api', etcRouter);
->>>>>>> origin/feat/back
 
 app.listen(port, () => {
     console.log(`Backend server is running on http://172.20.0.3:${port}`);
